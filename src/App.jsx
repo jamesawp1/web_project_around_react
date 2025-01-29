6e4ca4a28126d34f793eb1676be2bfe6bca8ac31
jamesawp1 <jamesawp@protonmail.com>
2025-01-29 11:13:10 -0300

    Replace the logo import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import logo from "./images/header__image.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,7 +11,7 @@ function App() {
         <header class="header">
           <img
             class="header__image"
-            src="./images/header__image.png"
+            src={logo}
             alt="Logo do site Around The Us."
           />
         </header>
@@ -70,147 +69,6 @@ function App() {
         <footer class="footer">
           <h2 class="footer__copyright">&copy; 2024 Around The U.S.</h2>
         </footer>
-
-        <div class="popup popup-edit-profile">
-          <div class="popup__container">
-            <img
-              class="popup__close-button"
-              src="./images/close_icon.png"
-              alt="Botão de fechar o popup"
-            />
-            <h2 class="popup__title">Editar perfil</h2>
-            <form class="popup__form">
-              <input
-                id="input-name"
-                class="popup__input"
-                type="text"
-                name="name"
-                placeholder="Nome"
-                minlength="2"
-                maxlength="40"
-                required
-              />
-              <span
-                id="input-name-error"
-                class="popup__form-error input-name-error"
-              ></span>
-              <input
-                id="input-job"
-                class="popup__input"
-                type="text"
-                name="about"
-                placeholder="Sobre Mim"
-                minlength="2"
-                maxlength="200"
-                required
-              />
-              <span
-                id="input-role-error"
-                class="popup__form-error input-job-error"
-              ></span>
-              <button id="save-button" class="popup__button" type="submit">
-                Salvar
-              </button>
-            </form>
-          </div>
-        </div>
-
-        <div class="popup popup-add-card">
-          <div class="popup__container">
-            <img
-              class="popup__close-button popup-add-card__close-button"
-              src="./images/close_icon.png"
-              alt="Botão de fechar o popup"
-            />
-            <h2 class="popup__title">Novo Lugar</h2>
-            <form class="popup__form popup__form-add-card">
-              <input
-                id="input-place-title"
-                class="popup__input"
-                name="name"
-                placeholder="Título"
-                minlength="2"
-                maxlength="30"
-                required
-              />
-              <span
-                id="input-place-title-error"
-                class="popup__form-error input-place-title-error"
-              ></span>
-              <input
-                id="input-place-url"
-                class="popup__input"
-                name="link"
-                placeholder="URL da imagem"
-                type="url"
-                required
-              />
-              <span
-                id="input-place-url-error"
-                class="popup__form-error input-place-url-error"
-              ></span>
-              <button type="submit" class="popup__button">
-                Salvar
-              </button>
-            </form>
-          </div>
-        </div>
-
-        <div class="popup popup-view-image">
-          <div class="popup__container popup-view-image__image-title-container">
-            <img
-              class="popup__close-button popup-view-image__close-button"
-              src="./images/close_icon.png"
-              alt="Botão de fechar o popup"
-            />
-            <div class="popup-view-image__image-title-container">
-              <img class="popup-view-image__image" src="#" alt="" />
-              <p class="popup-view-image__title"></p>
-            </div>
-          </div>
-        </div>
-
-        <div class="popup popup-profile-picture">
-          <div class="popup__container">
-            <img
-              class="popup__close-button popup-profile-picture__close-button"
-              src="./images/close_icon.png"
-              alt="Botão de fechar o popup"
-            />
-            <h2 class="popup__title">Alterar a foto do perfil</h2>
-            <form class="popup__form popup__profile-picture">
-              <input
-                id="input-profile-picture"
-                class="popup__input"
-                name="link"
-                placeholder="URL da fotografia"
-                type="url"
-                required
-              />
-              <span
-                id="input-profile-picture-error"
-                class="popup__form-error input-profile-picture-error"
-              ></span>
-              <button type="submit" class="popup__button">
-                Salvar
-              </button>
-            </form>
-          </div>
-        </div>
-
-        <div class="popup popup-confirmation">
-          <div class="popup__container popup__container-confirmation">
-            <img
-              class="popup__close-button popup-confirmation__close-button"
-              src="./images/close_icon.png"
-              alt="Botão de fechar o popup"
-            />
-            <h2 class="popup__title">Tem certeza?</h2>
-            <button type="submit" class="popup__button">
-              Sim
-            </button>
-          </div>
-        </div>
       </div>
     </>
   );
