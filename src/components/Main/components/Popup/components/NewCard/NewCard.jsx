d5359dfb609343510ff7e8541c9c15f1f9ec28de
jamesawp1 <jamesawp@protonmail.com>
2025-01-30 09:35:29 -0300

    feature: create NewCard component

---
+++ src/components/Main/components/Popup/components/NewCard/NewCard.jsx
@@ -0,0 +1,44 @@
+export default function NewCard() {
+  return (
+    <form
+      className="popup__form"
+      name="card-form"
+      id="new-card-form"
+      noValidate
+    >
+      <label className="popup__field">
+        <input
+          id="card-name"
+          className="popup__input popup__input_type_card-name"
+          maxLength="30"
+          minLength="1"
+          name="card-name"
+          placeholder="Título"
+          required
+          type="text"
+        />
+        <span
+          id="card-name-error"
+          className="popup__error card-name-error"
+        ></span>
+      </label>
+      <label className="popup__field">
+        <input
+          id="card-link"
+          className="popup__input popup__input_type_url"
+          name="link"
+          placeholder="URL da imagem"
+          type="url"
+          required
+        />
+        <span
+          id="card-link-error"
+          className="popup__error card-link-error"
+        ></span>
+      </label>
+      <button type="submit" className="button popup__button">
+        Salvar
+      </button>
+    </form>
+  );
+}
