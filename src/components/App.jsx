0879894721511ee9a4de962cc22495c11adf636a
jamesawp1 <jamesawp@protonmail.com>
2025-02-16 09:58:20 -0300

    Create the currentUser context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,30 @@
 import Header from "./Header/Header.jsx";
 import Main from "./Main/Main.jsx";
 import Footer from "./Footer/Footer.jsx";
+import { useState, useEffect } from "react";
+import { api } from "../utils/api.js";
+import { CurrentUserContext } from "../context/CurrentUserContext.js";
+
 function App() {
+  const [currentUser, setCurrentUser] = useState([]);
+  async function handleGetUser() {
+    const response = await api.getInitialUserInfo();
+    const userResponse = await response.json();
+  }
+  useEffect(() => {
+    handleGetUser();
+  }, []);
+
   return (
     <>
       <div className="page">
-        <Header></Header>
+        <CurrentUserContext.Provider value={currentUser}>
+          <Header></Header>
 
-        <Main></Main>
+          <Main></Main>
 
-        <Footer></Footer>
+          <Footer></Footer>
+        </CurrentUserContext.Provider>
       </div>
     </>
   );
